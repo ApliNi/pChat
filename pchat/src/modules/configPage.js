62de971511a8bd6f5a3fed5a63b156a21defc71d
ApliNi <aplini@ipacel.cc>
2026-03-03 13:03:37 +0800

    修改 WebDAV 清理按钮文本

--- pchat/src/modules/configPage.js
+++ pchat/src/modules/configPage.js
@@ -68,7 +68,7 @@ document.querySelector('#config .content').innerHTML = /*html*/`
 <p>
 	<span>目录: <code>./pChat/sync/{日期}/{会话文件}</code></span>
 	<button id="webdav-sync-btn" title="立即运行同步">[RUN_SYNC]</button>
-	<button id="webdav-cleanup-btn" title="清理远程存储中被标记为删除的数据">[CLEAN_DELETE_MARK]</button>
+	<button id="webdav-cleanup-btn" title="清理远程存储中被标记为删除的数据">[CLEAN]</button>
 </p>
 
 <p id="webdavSyncStatus" style="color: var(--text-color-muted);">同步未开始</p>
